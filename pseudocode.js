f5497dabeef2303a81623d4239bed2976406117b
Patricia Correia <correiaph13@gmail.com>
2019-06-08 12:46:49 -0500

    basic html page, some pseudocode

---
+++ pseudocode.js
@@ -0,0 +1,6 @@
+// Index.html
+// css styling, either with bootstrap, materialize or foundation
+// dropdowns - data get from? Array ourselves
+// pass data to college API (college scorecard);
+// on selection of college, switch to secondary selection search page
+// add more buttons (grayed out); 
