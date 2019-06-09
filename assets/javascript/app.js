3720864e2836f19f72d0cecb017dcd9e9eded7b9
Patricia <correiaph13@gmail.com>
2019-06-09 14:57:01 -0500

    inital directory structure and files added, some html skeleton added, js pseudocode started

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var yelpAPiKey = "fFJlb-TgllThsLcul6K4EZTazDF13bXIjnmtgr1hQaBFkenqGhL-8mZqCJwSWecsRfCVXOtCmIeYk-tHXPtgZCBoLRLatbyueBAS0NGl2Hbl4-LD9uwyvuaAZdf7XHYx"
+var yelpClientID = "QBIoy42JeuTrF4OPiyOP1Q"
+
+var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii","Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('.carousel').carousel()
+});
+
