ccd88a3995b18380910f978c288839777f91d30a
Patricia Correia <correiaph13@gmail.com>
2019-06-12 19:44:14 -0500

    changing API variable authy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,26 @@
-var yelpAPiKey = "fFJlb-TgllThsLcul6K4EZTazDF13bXIjnmtgr1hQaBFkenqGhL-8mZqCJwSWecsRfCVXOtCmIeYk-tHXPtgZCBoLRLatbyueBAS0NGl2Hbl4-LD9uwyvuaAZdf7XHYx"
-var yelpClientID = "QBIoy42JeuTrF4OPiyOP1Q"
+var clientIDFoursquare ="QIQOY4DP1NMCYBBDMXYYQOYI0TQDGUX0WJRR5QUJZYV2NLUD";
 
 var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii","Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $('.carousel').carousel()
-});
 
+var queryURL = ""
+
+
+
+var myurl = "";
+
+$( document ).ready(function() {
+
+         $.ajax({
+            url: myurl,
+            headers: {
+             'Authorization':'Bearer QBIoy42JeuTrF4OPiyOP1Q',
+         },
+            method: 'GET',
+            dataType: 'json',
+            success: function(data){
+                console.log('success: '+data);
+            }
+         });   
+
+      console.log("js is working");
+        });   
