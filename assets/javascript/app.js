16b70f511a70d4a242b5b4648f1bcc069bca122d
TerrenceMM2 <terrencemm2@gmail.com>
2019-06-18 08:36:08 -0500

    adds hover.css, animate.css, and loaders.css; waits until all colleges are loaded before showing second dropdown menu

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -233,7 +233,6 @@ $(document).ready(function () {
       $("#school-selection").empty();
       searchInput = $(this).val();
       $("#school-selection").append($("<option>").text("Choose an Institution"));
-      $("#school-group").show();
 
       getInstitution(dataGovPageNumber);
 
@@ -270,10 +269,12 @@ $(document).ready(function () {
             });
 
          });
- 
+
       });
 
       function getInstitution(num) {
+         $(".ball-pulse-sync").show();
+         $("#school-group").hide()
          $.ajax({
             url: `https://api.data.gov/ed/collegescorecard/v1/schools?api_key=B8ab3aPW1VmB52ZmHPMpgyftQXVVd1aRiDbYbnxl&school.state=${searchInput}&_fields=school.name,id&_per_page=100&_page=${dataGovPageNumber}&_sort=school.name`,
             method: "GET"
@@ -296,14 +297,19 @@ $(document).ready(function () {
             // The function calls itself if there are more schools than the result query max of 100.
             if (resultsTotal < totalNumberOfSchools) {
                getInstitution(num);
+               
+            } else {
+               $(".ball-pulse-sync").hide();
+               $("#school-group").attr("class", "animated zoomIn faster")
+               $("#school-group").css("display", "flex");
             }
          });
       };
 
    });
 
-//On-Click function for when event topic card is clicked. Making call to FourSquare API for fitness in the area.
-   $("#fitnessButton").on("click", function(){
+   //On-Click function for when event topic card is clicked. Making call to FourSquare API for fitness in the area.
+   $("#fitnessButton").on("click", function () {
       event.preventDefault();
       $("#resultsDiv").empty();
       query = "fitness";
@@ -322,128 +328,127 @@ $(document).ready(function () {
             limit: responsesLimit
          }
       }).then(function (response) {
-         for (var i=0;i<response.response.groups[0].items.length;i++){
+         for (var i = 0; i < response.response.groups[0].items.length; i++) {
             fitnessResults.push(response.response.groups[0].items[i].venue.name);
             locationAddress.push(response.response.groups[0].items[i].venue.location.formattedAddress[0])
          }
          console.log(response.response.groups[0].items);
          console.log(fitnessResults);
          console.log(locationAddress);
-         for (var j=0;j<fitnessResults.length;j++){
-            var newCard = $("<div class='row'><div class=card  style='width: 18rem;'><div class='card-body' data-fitness="+j+"><img src='https://assets.dmagstatic.com/wp-content/uploads/2019/01/iStock-871070868-677x451.jpg' class='card-img-top' alt='event-image'><h5 class='card-title mt-2'>"+fitnessResults[j]+"</h5><p class='card-text'>"+locationAddress[j]+"</p></div></div></div>")
+         for (var j = 0; j < fitnessResults.length; j++) {
+            var newCard = $("<div class='row'><div class=card  style='width: 18rem;'><div class='card-body' data-fitness=" + j + "><img src='https://assets.dmagstatic.com/wp-content/uploads/2019/01/iStock-871070868-677x451.jpg' class='card-img-top' alt='event-image'><h5 class='card-title mt-2'>" + fitnessResults[j] + "</h5><p class='card-text'>" + locationAddress[j] + "</p></div></div></div>")
             $("#resultsDiv").append(newCard);
          }
       });
 
    });
 
-   
-//On-Click function for when event topic card is clicked. Making call to FourSquare API for restaurants in the area.
-$("#foodButton").on("click", function(){
-   event.preventDefault();
-   $("#resultsDiv").empty();
-   query = "restaurants";
-   schoolZip = "37240";
-   var queryURL = `https://api.foursquare.com/v2/venues/explore?client_id=${clientIDFoursquare}&client_secret=${clientSecret}&v=20180323&limit=${responsesLimit}&near=${schoolZip}&query=${query}`;
-   var locationAddress = [];
-   $.ajax({
-      url: queryURL,
-      method: 'GET',
-      dataType: 'json',
-      qs: {
-         client_id: clientIDFoursquare,
-         client_secret: clientSecret,
-         near: schoolZip,
-         query: query,
-         limit: responsesLimit
-      }
-   }).then(function (response) {
-      for (var i=0;i<response.response.groups[0].items.length;i++){
-         foodResults.push(response.response.groups[0].items[i].venue.name);
-         locationAddress.push(response.response.groups[0].items[i].venue.location.formattedAddress[0])
-      }
-      console.log(response.response.groups[0].items);
-      console.log(foodResults);
-      console.log(locationAddress);
-      for (var j=0;j<foodResults.length;j++){
-         var newCard = $("<div class='row'><div class=card  style='width: 18rem;'><div class='card-body' data-food="+j+"><img src='https://www.studentbrands.co.za/wp-content/uploads/2016/05/2D_SpitBill.jpg' class='card-img-top' alt='event-image'><h5 class='card-title mt-2'>"+foodResults[j]+"</h5><p class='card-text'>"+locationAddress[j]+"</p></div></div></div>")
-         $("#resultsDiv").append(newCard);
-      }
+
+   //On-Click function for when event topic card is clicked. Making call to FourSquare API for restaurants in the area.
+   $("#foodButton").on("click", function () {
+      event.preventDefault();
+      $("#resultsDiv").empty();
+      query = "restaurants";
+      schoolZip = "37240";
+      var queryURL = `https://api.foursquare.com/v2/venues/explore?client_id=${clientIDFoursquare}&client_secret=${clientSecret}&v=20180323&limit=${responsesLimit}&near=${schoolZip}&query=${query}`;
+      var locationAddress = [];
+      $.ajax({
+         url: queryURL,
+         method: 'GET',
+         dataType: 'json',
+         qs: {
+            client_id: clientIDFoursquare,
+            client_secret: clientSecret,
+            near: schoolZip,
+            query: query,
+            limit: responsesLimit
+         }
+      }).then(function (response) {
+         for (var i = 0; i < response.response.groups[0].items.length; i++) {
+            foodResults.push(response.response.groups[0].items[i].venue.name);
+            locationAddress.push(response.response.groups[0].items[i].venue.location.formattedAddress[0])
+         }
+         console.log(response.response.groups[0].items);
+         console.log(foodResults);
+         console.log(locationAddress);
+         for (var j = 0; j < foodResults.length; j++) {
+            var newCard = $("<div class='row'><div class=card  style='width: 18rem;'><div class='card-body' data-food=" + j + "><img src='https://www.studentbrands.co.za/wp-content/uploads/2016/05/2D_SpitBill.jpg' class='card-img-top' alt='event-image'><h5 class='card-title mt-2'>" + foodResults[j] + "</h5><p class='card-text'>" + locationAddress[j] + "</p></div></div></div>")
+            $("#resultsDiv").append(newCard);
+         }
+      });
+
+   });
+
+
+   //On-Click function for when event topic card is clicked. Making call to FourSquare API for restaurants in the area.
+   $("#shopButton").on("click", function () {
+      event.preventDefault();
+      $("#resultsDiv").empty();
+      query = "shopping";
+      schoolZip = "37240";
+      var queryURL = `https://api.foursquare.com/v2/venues/explore?client_id=${clientIDFoursquare}&client_secret=${clientSecret}&v=20180323&limit=${responsesLimit}&near=${schoolZip}&query=${query}`;
+      var locationAddress = [];
+      $.ajax({
+         url: queryURL,
+         method: 'GET',
+         dataType: 'json',
+         qs: {
+            client_id: clientIDFoursquare,
+            client_secret: clientSecret,
+            near: schoolZip,
+            query: query,
+            limit: responsesLimit
+         }
+      }).then(function (response) {
+         for (var i = 0; i < response.response.groups[0].items.length; i++) {
+            shopResults.push(response.response.groups[0].items[i].venue.name);
+            locationAddress.push(response.response.groups[0].items[i].venue.location.formattedAddress[0])
+         }
+         console.log(response.response.groups[0].items);
+         console.log(shopResults);
+         console.log(locationAddress);
+         for (var j = 0; j < shopResults.length; j++) {
+            var newCard = $("<div class='row'><div class=card  style='width: 18rem;'><div class='card-body' data-shop=" + j + "><img src='https://www.cbc.ca/parents/content/imgs/kidsatconcerts_lead_emissio.jpg' class='card-img-top' alt='event-image'><h5 class='card-title mt-2'>" + shopResults[j] + "</h5><p class='card-text'>" + locationAddress[j] + "</p></div></div></div>")
+            $("#resultsDiv").append(newCard);
+         }
+      });
+
+   });
+
+
+   //On-Click function for when event topic card is clicked. Making call to FourSquare API for restaurants in the area.
+   $("#parksButton").on("click", function () {
+      event.preventDefault();
+      $("#resultsDiv").empty();
+      query = "parks";
+      schoolZip = "37240";
+      var queryURL = `https://api.foursquare.com/v2/venues/explore?client_id=${clientIDFoursquare}&client_secret=${clientSecret}&v=20180323&limit=${responsesLimit}&near=${schoolZip}&query=${query}`;
+      var locationAddress = [];
+      $.ajax({
+         url: queryURL,
+         method: 'GET',
+         dataType: 'json',
+         qs: {
+            client_id: clientIDFoursquare,
+            client_secret: clientSecret,
+            near: schoolZip,
+            query: query,
+            limit: responsesLimit
+         }
+      }).then(function (response) {
+         for (var i = 0; i < response.response.groups[0].items.length; i++) {
+            parkResults.push(response.response.groups[0].items[i].venue.name);
+            locationAddress.push(response.response.groups[0].items[i].venue.location.formattedAddress[0])
+         }
+         console.log(response.response.groups[0].items);
+         console.log(parkResults);
+         console.log(locationAddress);
+         for (var j = 0; j < parkResults.length; j++) {
+            var newCard = $("<div class='row'><div class=card  style='width: 18rem;'><div class='card-body' data-park=" + j + "><img src='https://www.studentbrands.co.za/wp-content/uploads/2016/05/2D_SpitBill.jpg' class='card-img-top' alt='event-image'><h5 class='card-title mt-2'>" + parkResults[j] + "</h5><p class='card-text'>" + locationAddress[j] + "</p></div></div></div>")
+            $("#resultsDiv").append(newCard);
+         }
+      });
+
    });
 
 });
-
- 
-//On-Click function for when event topic card is clicked. Making call to FourSquare API for restaurants in the area.
-$("#shopButton").on("click", function(){
-   event.preventDefault();
-   $("#resultsDiv").empty();
-   query = "shopping";
-   schoolZip = "37240";
-   var queryURL = `https://api.foursquare.com/v2/venues/explore?client_id=${clientIDFoursquare}&client_secret=${clientSecret}&v=20180323&limit=${responsesLimit}&near=${schoolZip}&query=${query}`;
-   var locationAddress = [];
-   $.ajax({
-      url: queryURL,
-      method: 'GET',
-      dataType: 'json',
-      qs: {
-         client_id: clientIDFoursquare,
-         client_secret: clientSecret,
-         near: schoolZip,
-         query: query,
-         limit: responsesLimit
-      }
-   }).then(function (response) {
-      for (var i=0;i<response.response.groups[0].items.length;i++){
-         shopResults.push(response.response.groups[0].items[i].venue.name);
-         locationAddress.push(response.response.groups[0].items[i].venue.location.formattedAddress[0])
-      }
-      console.log(response.response.groups[0].items);
-      console.log(shopResults);
-      console.log(locationAddress);
-      for (var j=0;j<shopResults.length;j++){
-         var newCard = $("<div class='row'><div class=card  style='width: 18rem;'><div class='card-body' data-shop="+j+"><img src='https://www.cbc.ca/parents/content/imgs/kidsatconcerts_lead_emissio.jpg' class='card-img-top' alt='event-image'><h5 class='card-title mt-2'>"+shopResults[j]+"</h5><p class='card-text'>"+locationAddress[j]+"</p></div></div></div>")
-         $("#resultsDiv").append(newCard);
-      }
-   });
-
-});
-
- 
-//On-Click function for when event topic card is clicked. Making call to FourSquare API for restaurants in the area.
-$("#parksButton").on("click", function(){
-   event.preventDefault();
-   $("#resultsDiv").empty();
-   query = "parks";
-   schoolZip = "37240";
-   var queryURL = `https://api.foursquare.com/v2/venues/explore?client_id=${clientIDFoursquare}&client_secret=${clientSecret}&v=20180323&limit=${responsesLimit}&near=${schoolZip}&query=${query}`;
-   var locationAddress = [];
-   $.ajax({
-      url: queryURL,
-      method: 'GET',
-      dataType: 'json',
-      qs: {
-         client_id: clientIDFoursquare,
-         client_secret: clientSecret,
-         near: schoolZip,
-         query: query,
-         limit: responsesLimit
-      }
-   }).then(function (response) {
-      for (var i=0;i<response.response.groups[0].items.length;i++){
-         parkResults.push(response.response.groups[0].items[i].venue.name);
-         locationAddress.push(response.response.groups[0].items[i].venue.location.formattedAddress[0])
-      }
-      console.log(response.response.groups[0].items);
-      console.log(parkResults);
-      console.log(locationAddress);
-      for (var j=0;j<parkResults.length;j++){
-         var newCard = $("<div class='row'><div class=card  style='width: 18rem;'><div class='card-body' data-park="+j+"><img src='https://www.studentbrands.co.za/wp-content/uploads/2016/05/2D_SpitBill.jpg' class='card-img-top' alt='event-image'><h5 class='card-title mt-2'>"+parkResults[j]+"</h5><p class='card-text'>"+locationAddress[j]+"</p></div></div></div>")
-         $("#resultsDiv").append(newCard);
-      }
-   });
-
-});
-
-});
-
