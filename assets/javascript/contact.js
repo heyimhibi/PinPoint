b030f4afcf0c33501a51a64268a38afe3274fc0d
wendym <ozmabeam@gmail.com>
2019-06-20 14:43:44 -0500

    need assistance checkin input validation

--- assets/javascript/contact.js
+++ assets/javascript/contact.js
@@ -17,27 +17,30 @@
   
 
     var userName = $("#name-input").val().trim();
-    var userEmail = $("#email-input").val().trim();
+    var userEmail1 = $("#email-input1").val().trim();
+    var userEmail2 = $("#email-input2").val().trim();
     var userMessage = $("#message-input").val().trim();
   
 
     var newUser = {
       name: userName,
-      email: userEmail,
+      email1: userEmail1,
+      email2: userEmail2,
       message: userMessage
     };
   
-
     database.ref().push(newUser);
   
 
     console.log(newUser.name);
-    console.log(newUser.email);
+    console.log(newUser.email1);
+    console.log(newUser.email2);
     console.log(newUser.message);
   
 
     $("#name-input").val("");
-    $("#email-input").val("");
+    $("#email-input1").val("");
+    $("#email-input2").val("");
     $("#message-input").val("");
   });
   
@@ -46,3 +49,10 @@
     console.log(childSnapshot.val());
   });
  
+  function BothFieldsIdenticalCaseSensitive(email1, email2) {
+    var two = document.getElementById(email-input1).value;
+    var three = document.getElementById(email-input2).value;
+    if(two == three) { return true; }
+    alert("Warning!! passcodes must match!!!");
+    return false;
+};
